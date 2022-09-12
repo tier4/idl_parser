3bed0323af10edb98293bc434afffb1bdf7b3463
Yuuki Takano <yuuki.takano@tier4.jp>
2022-09-12 18:05:54 +0900

    parse comment, keywords, and decimal
    Signed-off-by: Yuuki Takano <yuuki.takano@tier4.jp>

--- src/character.rs
+++ src/character.rs
@@ -1,17 +1,5 @@
-pub const HT: u8 = 0x09; // horizontal tab
-pub const VT: u8 = 0x0b; // vertical tab
-pub const LF: u8 = 0x0a; // line feed, new line
-pub const FF: u8 = 0x0c; // form feed
-pub const SP: u8 = 0x20; // white space
-
-pub fn is_latin1(c: u8) -> bool {
-    matches!(c, 0x20..=0x7e | 0xa0..=0xff)
-}
-
-pub fn is_token(c: u8) -> bool {
-    matches!(c, SP | HT | VT | FF)
-}
-
-pub fn is_space(c: u8) -> bool {
-    c == SP
-}
+pub const HT: &str = "\t"; // horizontal tab
+pub const VT: &str = "\u{000B}"; // vertical tab
+pub const LF: &str = "\n"; // line feed, new line
+pub const FF: &str = "\u{000c}"; // form feed
+pub const SP: &str = " "; // white space
--- src/parser.rs
+++ src/parser.rs
@@ -1,21 +1,162 @@
-use nom::{branch::alt, bytes::complete::tag, error::VerboseError, multi::many0, IResult};
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_until1, take_while1},
+    character::{
+        complete::{digit0, one_of},
+        is_alphanumeric,
+    },
+    combinator::eof,
+    error::VerboseError,
+    multi::many0,
+    IResult,
+};
+use num_bigint::BigInt;
+use num_traits::Zero;
 
-use crate::character::SP;
+use crate::character::LF;
 
-type PResult<'a, OUT> = IResult<&'a [u8], OUT, VerboseError<&'a [u8]>>;
+type PResult<'a, OUT> = IResult<&'a str, OUT, VerboseError<&'a str>>;
 
 // fn parse_comment(input: &str) -> PResult<Expr> {}
 
-fn parse_comment(input: &[u8]) -> PResult<String> {
+fn parse_comment(input: &str) -> PResult<&str> {
     skip_space0(input)?;
 
     let (input, c) = alt((tag("//"), tag("/*")))(input)?;
 
-    todo!();
-    // Ok("a".to_string())
+    let (input, comment) = match c {
+        "//" => alt((tag(LF), eof))(input)?,
+        "/*" => {
+            let (input, c) = take_until1("*/")(input)?;
+            let (input, _) = tag("*/")(input)?;
+            (input, c)
+        }
+        _ => unreachable!(),
+    };
+
+    Ok((input, comment))
 }
 
-fn skip_space0(input: &[u8]) -> PResult<()> {
+fn parse_id(input: &str) -> PResult<&str> {
+    take_while1(|c| c == '_' || is_alphanumeric(c as u8))(input)
+}
+
+fn skip_space0(input: &str) -> PResult<()> {
     let (input, _) = many0(tag(" "))(input)?;
     Ok((input, ()))
+}
+
+fn parse_keywards(input: &str) -> PResult<&str> {
+    alt((
+        alt((
+            tag("abstract"),
+            tag("any"),
+            tag("alias"),
+            tag("attribute"),
+            tag("bitfield"),
+            tag("bitmask"),
+            tag("bitset"),
+            tag("boolean"),
+            tag("case"),
+            tag("char"),
+            tag("component"),
+            tag("connector"),
+            tag("const"),
+            tag("consumes"),
+            tag("context"),
+            tag("custom"),
+        )),
+        alt((
+            tag("default"),
+            tag("double"),
+            tag("exception"),
+            tag("emits"),
+            tag("enum"),
+            tag("eventype"),
+            tag("factory"),
+            tag("FALSE"),
+            tag("finder"),
+            tag("fixed"),
+            tag("float"),
+            tag("getraises"),
+            tag("home"),
+            tag("import"),
+            tag("in"),
+            tag("inout"),
+        )),
+        alt((
+            tag("interface"),
+            tag("local"),
+            tag("long"),
+            tag("manages"),
+            tag("map"),
+            tag("mirrorport"),
+            tag("module"),
+            tag("multiple"),
+            tag("native"),
+            tag("Object"),
+            tag("octet"),
+            tag("oneway"),
+            tag("out"),
+            tag("primarykey"),
+            tag("private"),
+            tag("port"),
+        )),
+        alt((
+            tag("porttype"),
+            tag("provides"),
+            tag("public"),
+            tag("publishes"),
+            tag("raises"),
+            tag("readonly"),
+            tag("setraises"),
+            tag("sequence"),
+            tag("short"),
+            tag("string"),
+            tag("struct"),
+            tag("supports"),
+            tag("switch"),
+            tag("TRUE"),
+            tag("truncatable"),
+            tag("typedef"),
+        )),
+        alt((
+            tag("typeid"),
+            tag("typename"),
+            tag("typeprefix"),
+            tag("unsigned"),
+            tag("union"),
+            tag("uses"),
+            tag("ValueBase"),
+            tag("valuetype"),
+            tag("void"),
+            tag("wchar"),
+            tag("wstring"),
+            tag("int8"),
+            tag("uint8"),
+            tag("int16"),
+            tag("int32"),
+            tag("int64"),
+            tag("uint16"),
+            tag("uint32"),
+            tag("uint64"),
+        )),
+    ))(input)
+}
+
+fn parse_decimal(input: &str) -> PResult<BigInt> {
+    let (input, head) = one_of("123456789")(input)?;
+    let (input, tail) = digit0(input)?;
+
+    let mut result = BigInt::zero();
+    let n = head as u8 - '0' as u8;
+    result += n;
+
+    for n in tail.as_bytes().iter() {
+        let val = *n - '0' as u8;
+        result *= 10;
+        result += val;
+    }
+
+    Ok((input, result))
 }
